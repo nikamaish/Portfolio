8aa0a43a1e76f3bc03691c59b4a63b50b4a99057
nikamaish <100349291+nikamaish@users.noreply.github.com>
2023-07-31 15:05:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Topbar from "./components/topbar/topbar";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import "./App.scss";
+import React from "react" ;
+import {useState} from "react";
+import  Menu from "./components/menu/Menu";
+
+
+function App() {
+
+  const [menuOpen,setMenuOpen]=React.useState(false)
+
+  return (
+    <div className="app">
+     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+  
+
+     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     
+     
+     <div className="sections">
+       <Intro/>
+       <Portfolio/>
+       <Works/>
+       <Testimonials/>
+       <Contact/>
+     </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,189 @@
+export const featuredPortfolio = [
+    {
+      id: 1,
+      title: "C",
+      img:
+        "https://w0.peakpx.com/wallpaper/987/583/HD-wallpaper-language-c-chedesign-programming-thumbnail.jpg",
+    },
+    {
+      id: 2,
+      title: "C++",
+      img:
+        "https://ih1.redbubble.net/image.1075465556.1060/pp,840x830-pad,1000x1000,f8f8f8.u4.jpg",
+    },
+    {
+      id: 3,
+      title: "DSA",
+      img:
+        "https://repository-images.githubusercontent.com/403817624/3d10f761-1027-4d0a-9906-48361e466d87",
+    },
+    // {
+    //   id: 4,
+    //   title: "Relax Mobile App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Hero Web Design",
+    //   img:
+    //     "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Banking App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+    // },
+  ];
+  
+  export const webPortfolio = [
+    {
+      id: 1,
+      title: "HTML",
+      img: "https://c4.wallpaperflare.com/wallpaper/887/817/276/html-5-html-5-logo-wallpaper-preview.jpg",
+    },
+    {
+      id: 2,
+      title: "CSS",
+      img: "https://images7.design-editor.com/90/9098746/3958/46d2d005-1b02-4961-aa1d-0aba7f7209bd.jpg",
+    },
+    {
+      id: 3,
+      title: "Javascript",
+      img: "https://www.freepnglogos.com/uploads/javascript-png/javascript-vector-logo-yellow-png-transparent-javascript-vector-12.png",
+    },
+    {
+      id: 4,
+      title: "React JS",
+      img: "https://shethink.in/wp-content/uploads/2021/07/react.js-img.png",
+    },
+    {
+      id: 5,
+      title: "BootStrap",
+      img: "https://www.brcline.com/wp-content/uploads/2016/01/bootstrap-logo.png",
+    },
+    // {
+    //   id: 6,
+    //   title: "Web Banking App",
+    //   img: "https://www.brcline.com/wp-content/uploads/2016/01/bootstrap-logo.png",
+    // },
+  ];
+  
+  export const mobilePortfolio = [
+    {
+      id: 1,
+      title: "Node JS",
+      img:
+        "https://miro.medium.com/v2/resize:fit:22584/1*WDX58nzlaiClqTFT59v7RQ.jpeg",
+    },
+    {
+      id: 2,
+      title: "Express JS",
+      img:
+        "https://miro.medium.com/v2/resize:fit:1160/0*skYUWg3ZvcY8xRf5",
+    },
+    {
+      id: 3,
+      title: "MySQL",
+      img:
+        "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png",
+    },
+    {
+      id: 4,
+      title: "MongoDB",
+      img:
+        "https://webimages.mongodb.com/_com_assets/cms/kuzt9r42or1fxvlq2-Meta_Generic.png",
+    },
+    // {
+    //   id: 5,
+    //   title: "Mobile Hero Design",
+    //   img:
+    //     "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Mobile Banking App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    // },
+  ];
+  
+  export const designPortfolio = [
+    {
+      id: 1,
+      title: "Design Social Media App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 2,
+      title: "Design Rampa UI Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 3,
+      title: "Design E-commerce Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 4,
+      title: "Design Relax Mobile App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 5,
+      title: "Design Keser Web Design",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+    {
+      id: 6,
+      title: "Design Banking App",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    },
+  ];
+  
+  export const contentPortfolio = [
+    {
+      id: 1,
+      title: "Git",
+      img:
+        "https://www.stickerpress.in/media/products/800x800/b66880fed2234b05b1264254b5fae4d1.jpg",
+    },
+    {
+      id: 2,
+      title: "GitHub",
+      img:
+        "https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png",
+    },
+    {
+      id: 3,
+      title: "MongoDB Compass",
+      img:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--3CbkZmAq--/c_imagga_scale,f_auto,fl_progressive,h_1080,q_auto,w_1080/https://dev-to-uploads.s3.amazonaws.com/i/nl5odr3413k5dddlz1dd.png",
+    },
+    {
+      id: 4,
+      title: "PostMan",
+      img:
+        "https://images.squarespace-cdn.com/content/v1/57c649658419c2380d1947be/1530749877859-XD498I2YH1GZ00CUYXQ5/Artboard+1.png?format=1000w",
+    },
+    {
+      id: 5,
+      title: "VS code",
+      img:
+        "https://code.visualstudio.com/opengraphimg/opengraph-blog.png",
+    },
+    // {
+    //   id: 6,
+    //   title: "Content Banking App",
+    //   img:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    // },
+  ];
+  
